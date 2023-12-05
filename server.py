1936792ae755a550fb525996fdf1d7b2139b0098
Christopher Knapp <chris.knapp@live.de>
2023-12-05 17:02:21 +0100

    change tables to reflect needed data models

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy.orm import joinedload
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://middleware:supersafepassword@localhost/meditation_db'
@@ -19,14 +20,22 @@ def predict():
 
 @app.route('/meditations', methods=['GET'])
 def get_meditation_sessions():
-    # Retrieve all meditation sessions from the database
-    meditation_sessions = MeditationSession.query.all()
+    try:
+        # Use joinedload to eager load the related SessionMeta
+        meditation_sessions = MeditationSession.query.options(joinedload(MeditationSession.session_meta)).all()
 
-    # Convert the sessions to a list of dictionaries
-    result = [session.to_dict() for session in meditation_sessions]
+        if not meditation_sessions:
+            return jsonify({'message': 'No meditation sessions found'}), 404
 
-    # Return the result as JSON
-    return jsonify(result)
+        result = {'meditation_sessions': []}
+        for session in meditation_sessions:
+            result['meditation_sessions'].append(session.to_dict())
+
+        return jsonify(result)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return jsonify({'error': 'Internal Server Error'}), 500
 
 @app.route('/meditation', methods=['POST'])
 def create_meditation_session():
@@ -34,11 +43,11 @@ def create_meditation_session():
     data = request.json
 
     # Create a new meditation session
-    session = MeditationSession(title=data['title'], duration=data['duration'])
+    # session = MeditationSession(title=data['title'], duration=data['duration'])
     
     # Add the session to the database
-    db.session.add(session)
-    db.session.commit()
+    # db.session.add(session)
+    # db.session.commit()
 
     # return success string 
     return "Successfully saved meditation session."
@@ -46,20 +55,58 @@ def create_meditation_session():
 class MeditationSession(db.Model):
     __tablename__ = 'meditation_sessions'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
+    device_id = db.Column(db.String(36), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
     duration = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    time_until_relaxation = db.Column(db.Integer, nullable=True)
+    min_heart_rate = db.Column(db.Integer, nullable=True)
+    max_heart_rate = db.Column(db.Integer, nullable=True)
+    avg_heart_rate = db.Column(db.Integer, nullable=True)
+    heart_rate_measurements = db.Column(db.JSON, nullable=False)
+    session_meta_id = db.Column(db.Integer, db.ForeignKey('session_meta.id'), nullable=False)
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'title': self.title,
-            'duration': self.duration,
-            'created_at': self.created_at.isoformat() if self.created_at else None
+            'session': {
+                'deviceId': self.device_id,
+                'date': self.date.isoformat() if self.date else None,
+                'duration': self.duration,
+                'timeUntilRelaxation': self.time_until_relaxation,
+                'minHeartRate': self.min_heart_rate,
+                'maxHeartRate': self.max_heart_rate,
+                'avgHeartRate': self.avg_heart_rate,
+                'heartRateMeasurements': self.heart_rate_measurements
+            },
+        }
+
+class SessionMeta(db.Model):
+    __tablename__ = 'session_meta'
+    id = db.Column(db.Integer, primary_key=True)
+    is_haptic_feedback_enabled = db.Column(db.Boolean, nullable=False)
+    sound = db.Column(db.String(255), nullable=True)
+    ambient = db.Column(db.String(255), nullable=True)
+    mandala = db.Column(db.String(255), nullable=True)
+    breathing_pattern = db.Column(db.JSON, nullable=False)  # Use JSON type for breathing pattern
+    breathing_pattern_multiplier = db.Column(db.Integer, nullable=False)
+    meditation_session = db.relationship('MeditationSession', backref='session_meta', uselist=False)
+
+    def to_dict(self):
+        return {
+            'isHapticFeedbackEnabled': self.is_haptic_feedback_enabled,
+            'sound': self.sound,
+            'ambient': self.ambient,
+            'mandala': self.mandala,
+            'breathingPattern': self.breathing_pattern,
+            'breathingPatternMultiplier': self.breathing_pattern_multiplier
         }
 
 
 
 # Run on port 5001
 if __name__ == '__main__':
+
+    # with app.app_context():
+    #     db.drop_all()
+    #     db.create_all()
+
     app.run(port=5001, debug=True, threaded=True, host='0.0.0.0') 
